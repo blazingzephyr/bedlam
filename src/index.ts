fef50132b3fc1ce2d41cf61f4b9a4cf0ba9fb13d
ZaBlazzingZephyrus <blazerzeif@gmail.com>
2023-05-25 16:17:04 +0700

    Add option to disable multithreading

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,13 @@
 
-import { fileURLToPath } from "url";
-import { Worker, isMainThread, parentPort } from "worker_threads";
-import { Client } from "@notionhq/client";
-import { DiscordManager } from "./core/discord_manager.js";
-import { NotionCallback, NotionManager } from "./core/notion_manager.js";
-import { sendEmbed } from "./core/embed.js";
-import { NotionSearchObject } from "./core/utility.js";
-import * as config from "./core/config.json" assert { type: "json" };
+import * as config from "./config.json" assert { type: "json" };
 
-/**
- * Data transfered from the Notion thread to the main one.
- */
-type NotionData =
+// Runs the application depending on the threading mode.
+// @ts-expect-error is used because of an error of the TypeScript analyser.
+if (config.default.use_workers)
 {
-	/**
-	 * Type of the message.
-	 */
-	type: string,
-
-	/**
-	 * Notion resources that have been updated.
-	 */
-	updated?: NotionSearchObject[],
-
-	/**
-	 * All Notion Search resources.
-	 */
-	objects?: NotionSearchObject[]
-};
-
-// Executes if this is the main thread.
-// Used for Discord message processing.
-if (isMainThread)
-{
-	// Create the Notion thread from the same file.
-	const file = fileURLToPath(import.meta.url);
-	const notionThread = new Worker(file);
-
-	// Discord onKilled event.
-	// Terminates the Notion thread when the Discord client shuts down.
-	const killed = async () => { await notionThread.terminate(); };
-
-	// Initialize the manager.
-	// @ts-expect-error is used because of an error of the TypeScript analyser.
-	const manager = new DiscordManager(config.default.discord, [killed]);
-	const log = await manager.initialize();
-
-	// Declare the secondary Notion client used on embed events.
-	// @ts-expect-error is used because of an error of the TypeScript analyser.
-	const secondary = new Client({ auth: config.default.notion.integration_token });
-
-	// Listen to the Notion updates.
-	notionThread.on("message", async (value: NotionData) =>
-	{
-		if (value.type == "updated")
-		{
-			sendEmbed(log, value.updated!, value.objects!, secondary);
-		}
-	});
+	await import("./use_workers.js");
 }
-// Executes if this is the second thread.
-// Used for Notion watching.
 else
 {
-	// Notion onUpdated event.
-	// Transfers data to the Discord thread.
-	const updated: NotionCallback<"updated"> = async (updated, objects, client) =>
-	{
-		parentPort!.postMessage({ type: "updated", updated, objects });
-	}
-
-	// Initialize the manager and watch.
-	// @ts-expect-error is used because of an error of the TypeScript analyser.
-	const manager = new NotionManager(config.default.notion, [], [updated]);
-	await manager.watch();
+	await import("./no_workers.js");
 }
---
+++ src/no_workers.ts
@@ -0,0 +1,25 @@
+
+import { DiscordManager } from "./core/discord_manager.js";
+import { NotionCallback, NotionManager } from "./core/notion_manager.js";
+import { sendEmbed } from "./core/embed.js";
+import * as config from "./config.json" assert { type: "json" };
+
+// Discord onKilled event.
+// Terminates the Notion thread when the Discord client shuts down.
+const killed = async () => process.kill(process.pid);
+
+// Initialize the manager.
+// @ts-expect-error is used because of an error of the TypeScript analyser.
+const discord = new DiscordManager(config.default.discord, [killed]);
+const log = await discord.initialize();
+
+// Notion onUpdated event.
+const updated: NotionCallback<"updated"> = async (updated, objects, client) =>
+{
+	sendEmbed(log, updated, objects, client);
+};
+
+// Initialize the manager and watch.
+// @ts-expect-error is used because of an error of the TypeScript analyser.
+const notion = new NotionManager(config.default.notion, [], [updated]);
+await notion.watch();
